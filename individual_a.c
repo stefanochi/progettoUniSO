a663646a3d0114db49fcab0ed9e0bc2039957494
Stefano <chistefano@gmail.com>
2018-03-21 14:23:30 -0700

    added generation of process for each individual

---
+++ individual_a.c
@@ -0,0 +1,6 @@
+#include "population.h"
+
+int main(int argc, char ** argv){
+    printf("process a started\n");
+    exit(0);
+}
---
+++ individual_b.c
@@ -0,0 +1,6 @@
+#include "population.h"
+
+int main(int argc, char ** argv){
+    printf("process b started\n");
+    exit(0);
+}
