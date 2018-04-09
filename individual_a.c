7ed2cced97d287f6ec2fca80e9570dd852c91222
Gabriele Dal Pino <gabriele.dalpino@edu.unito.it>
2018-04-09 17:00:00 +0200

    added shm for b

--- individual_a.c
+++ individual_a.c
@@ -1,6 +1,5 @@
 #include "population.h"
 
 int main(int argc, char ** argv){
-    printf("process a started\n");
     exit(0);
 }
--- individual_b.c
+++ individual_b.c
@@ -1,6 +1,12 @@
-#include "population.h"
+#include <unistd.h>
+#include "shm.h"
 
 int main(int argc, char ** argv){
-    printf("process b started\n");
-    exit(0);
+  population * pop;
+  pop = createAttach (getppid(), 0);
+
+  individual* ind_list;
+  ind_list = (individual*) pop + sizeof(population);
+
+  exit(0);
 }
